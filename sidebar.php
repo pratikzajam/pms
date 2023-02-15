26f577a9541f34694f06d24f4d80d1b985b16a48
PratikZajam <zajampratik@gmail.com>
2023-02-15 17:15:50 +0530

    edit & delete functinaity sucessfully implemented

---
+++ sidebar.php
@@ -0,0 +1,26 @@
+<?php include('config.php');?>
+ <!-- ======= Sidebar ======= -->
+ <aside id="sidebar" class="sidebar">
+
+<ul class="sidebar-nav" id="sidebar-nav">
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="index.php">
+      <i class="bi bi-grid"></i>
+      <span>Add Patient</span>
+    </a>
+  </li><!-- End Dashboard Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="registered_patients.php">
+      <i class="bi bi-grid"></i>
+      <span>Registered patients</span>
+    </a>
+  </li><!-- End Dashboard Nav -->
+
+ 
+   
+
+</ul>
+
+</aside><!-- End Sidebar-->
