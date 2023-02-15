aa66a2d29da7cf5c980d3b3b37f4c387bb308bda
PratikZajam <zajampratik@gmail.com>
2023-02-15 11:53:55 +0530

    first commit

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php 
+$con=mysqli_connect("localhost","root","","pms");
+
+if(mysqli_connect_error())
+{
+ echo "Database is not connected".mysqli_connect_error();
+}
+else{
+echo"connection sucessful.";
+}
+
+
+//code for for login?
+
+
+
+?>
